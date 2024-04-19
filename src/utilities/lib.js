d17696cb5c11b121e30a674407e0da36d845d226
johanna-konova <167417772+johanna-konova@users.noreply.github.com>
2024-04-19 16:59:57 +0300

    Add files via upload

---
+++ src/utilities/lib.js
@@ -0,0 +1,14 @@
+import page from "//unpkg.com/page/page.mjs";
+import { html, render } from 'https://esm.run/lit-html@1';
+//import { ifDefined } from "//unpkg.com/lit-html/directives/if-defined?module";
+
+/*import page from "./../../node_modules/page/page.mjs";
+import { html, render } from "./../../node_modules/lit-html/lit-html.js";
+import { ifDefined } from "./../../node_modules/lit-html/directives/if-defined.js";*/
+
+export {
+    page,
+    html,
+    render,
+    //ifDefined,
+};
