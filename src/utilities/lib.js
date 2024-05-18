97693a70c865cff3d056decf3bcbb0462daff7aa
johanna-konova <j.konova@abv.bg>
2024-05-18 10:40:03 +0300

    commit "update page and html cdns"

--- src/utilities/lib.js
+++ src/utilities/lib.js
@@ -1,6 +1,6 @@
 import page from '//unpkg.com/page/page.mjs';
-import { html, render } from '//unpkg.com/lit-html?module';
-import { until } from '//unpkg.com/lit-html/directives/until?module';
+import { html, render } from 'https://esm.sh/lit-html';
+import { until } from 'https://esm.sh/lit-html/directives/until';
 
 /*import page from "./../../node_modules/page/page.mjs";
 import { html, render } from "./../../node_modules/lit-html/lit-html.js";
